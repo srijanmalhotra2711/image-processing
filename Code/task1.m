498070985cd551b881e9f9381a73a8549989a02c
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-06-26 17:19:36 +0530

    26th June 2021

---
+++ Code/task1.m
@@ -0,0 +1,23 @@
+I = imread('C:\Users\Lenovo\Pictures\butterfly.jpg');
+figure
+imshow(I)
+J = imnoise(I,'salt & pepper',0.02);
+figure
+imshow(J)
+b = imsharpen(J);
+figure, imshow(b)
+title('Sharpened Image')
+H = rgb2gray(J);
+figure
+imshow(H)
+hy = fspecial('sobel');
+hx = hy';
+Iy = imfilter(double(H), hy, 'replicate');
+Ix = imfilter(double(H), hx, 'replicate');
+gradmag = sqrt(Ix.^2 + Iy.^2);
+figure
+imshow(gradmag,[]), title('Gradient magnitude (gradmag)')
+X=imadd(I,J)
+figure,imshow(X)
+Z=imabsdiff(I,J)
+figure,imshow(Z)
