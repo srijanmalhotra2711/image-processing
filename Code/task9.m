b3f618c42aa27e37f6a947e0d2c69c82c1ad2648
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-06-26 17:20:46 +0530

    26th June 2021

---
+++ Code/task9.m
@@ -0,0 +1,12 @@
+A=imread('C:\Users\Lenovo\Pictures\tiger.jpg');
+A=im2bw(A);
+%Stucturing element
+B=[1 1 1 1 1 1 1;];
+C=padarray(A,[0,3]);
+D=false(size(A));
+for i=1: size(C,1)
+    for j=1:size(C,2)-6
+        D(i,j)=sum(B&C(i,j:j+6));
+    end
+end
+figure,imshow(D);
